93fc8b175d75ed4f0c24812a11647b803cce8fbb
Gayanga <gayangask@gmail.com>
2025-05-31 18:39:46 +0530

    initialized

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function showPass(){
+    let inputPass = document.getElementById("password");
+    if(inputPass.type === "password"){
+        inputPass.type = "text";
+    }else{
+        inputPass.type = "password";
+    }
+}
